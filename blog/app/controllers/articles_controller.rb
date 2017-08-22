7ecb597b4f428bc7a663dccd5731acda8ec980bc
John Roche <roche.johnjr@gmail.com>
2017-08-22 13:52:18 -0400

    destroy, comments, back

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,13 +1,17 @@
 class ArticlesController < ApplicationController
   def index
-   @articles = Article.all
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   def new
     @article = Article.new
   end
 
-  def show
+  def edit
     @article = Article.find(params[:id])
   end
 
@@ -19,6 +23,23 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+  end
+end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
  private
